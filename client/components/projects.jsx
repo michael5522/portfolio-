dc2104e3f27065d73bcb591504ed1cadc722126e
michael5522 <michaelchang.kuo@gmail.com>
2019-12-22 03:25:54 +0000

    finished projects

--- client/components/projects.jsx
+++ client/components/projects.jsx
@@ -2,6 +2,97 @@ import React from 'react';
 
 export default function Projects() {
   return (
-    <div className="full-view" id="projects">Projects</div>
+    <div className="project-bg d-flex justify-content-center align-items-center" id="projects">
+      <div className="container">
+        <h2 className="text-center">Projects</h2>
+        <div className="row">
+
+          <div className="col-lg-4 col-md-6 p-2">
+            <div className="card">
+              <div className="overflow-hidden">
+                <img src="./images/memory-match.png" className=" img-fluid grow" />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Nova Memory Match</h5>
+                <label className="text-muted">Js apps</label>
+                <p className="card-text">teemo teemo teemo teemo teemo
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 p-2">
+            <div className="card">
+              <div className="overflow-hidden">
+                <img src="./images/memory-match.png" className=" img-fluid grow" />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Nova Memory Match</h5>
+                <label className="text-muted">Js apps</label>
+                <p className="card-text">teemo teemo teemo teemo teemo
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 p-2">
+            <div className="card">
+              <div className="overflow-hidden">
+                <img src="./images/memory-match.png" className=" img-fluid grow" />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Nova Memory Match</h5>
+                <label className="text-muted">Js apps</label>
+                <p className="card-text">teemo teemo teemo teemo teemo
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 p-2">
+            <div className="card">
+              <div className="overflow-hidden">
+                <img src="./images/memory-match.png" className=" img-fluid grow" />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Nova Memory Match</h5>
+                <label className="text-muted">Js apps</label>
+                <p className="card-text">teemo teemo teemo teemo teemo
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 p-2">
+            <div className="card">
+              <div className="overflow-hidden">
+                <img src="./images/memory-match.png" className=" img-fluid grow" />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Nova Memory Match</h5>
+                <label className="text-muted">Js apps</label>
+                <p className="card-text">teemo teemo teemo teemo teemo
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 p-2">
+            <div className="card">
+              <div className="overflow-hidden">
+                <img src="./images/memory-match.png" className=" img-fluid grow" />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Nova Memory Match</h5>
+                <label className="text-muted">Js apps</label>
+                <p className="card-text">teemo teemo teemo teemo teemo
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   );
 }
