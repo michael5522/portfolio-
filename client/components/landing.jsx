6706c4446c7dfd97cfbb53d0d89f323acc3ac8f2
michael5522 <michaelchang.kuo@gmail.com>
2019-12-22 02:39:24 +0000

    finished about me

---
+++ client/components/landing.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Landing() {
+  return (
+    <div>
+      <div className=" bg-landing d-flex justify-content-center align-items-center" id="home"></div>
+
+    </div>
+  );
+}
